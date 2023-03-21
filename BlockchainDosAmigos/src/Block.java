b254701303b2cb14ade17edcc0e1f34314fc64c9
Gloower <gloowerzin@gmail.com>
2023-03-21 00:26:56 -0300

    Testes com Blockchain

--- BlockchainDosAmigos/src/Block.java
+++ BlockchainDosAmigos/src/Block.java
@@ -1,5 +1,141 @@
+import org.junit.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+
+import java.nio.charset.StandardCharsets;
+import java.nio.charset.UnsupportedCharsetException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.*;
+import java.util.logging.Logger;
+
+import static org.junit.Assert.assertTrue;
+
+
 public class Block {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    private String hash;
+    private String previousHash;
+    private String data;
+    private long timeStamp;
+    private int nonce;
+
+    public Block(String data, String previousHash, long timeStamp) {
+        this.data = data;
+        this.previousHash = previousHash;
+        this.timeStamp = timeStamp;
+        this.hash = calculateBlockHash();
     }
+
+    public String getHash() {
+        return hash;
+    }
+
+    public void setHash(String hash) {
+        this.hash = hash;
+    }
+
+    public String getPreviousHash() {
+        return previousHash;
+    }
+
+    public void setPreviousHash(String previousHash) {
+        this.previousHash = previousHash;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public long getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(long timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public int getNonce() {
+        return nonce;
+    }
+
+    public void setNonce(int nonce) {
+        this.nonce = nonce;
+    }
+
+
+
+
+    public String calculateBlockHash(){
+        Logger logger
+                = Logger.getLogger(
+                        Block.class.getName());
+
+        String dataToHash = previousHash
+                + Long.toString(timeStamp)
+                + Integer.toString(nonce)
+                + data;
+        MessageDigest digest = null;
+        byte[] bytes = null;
+        try {
+            digest = MessageDigest.getInstance("SHA-256");
+            bytes = digest.digest(dataToHash.getBytes(StandardCharsets.UTF_8));
+        }catch (NoSuchAlgorithmException | UnsupportedCharsetException ex){
+            logger.log(Level.SEVERE, ex.getMessage());
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        for (byte b : bytes) {
+            buffer.append(String.format("%02x", b));
+        }
+
+        return buffer.toString();
+    }
+
+
+    public String mineBlock(int prefix) {
+        String prefixString = new String(new char[prefix]).replace('\0', '0');
+        while (!hash.substring(0, prefix).equals(prefixString)){
+            nonce++;
+            hash = calculateBlockHash();
+
+        }
+        return hash;
+    }
+
+
+    List<Block> blockchain = new ArrayList<>();
+    int prefix = 4;
+    String prefixString = new String(new char[prefix]).replace('\0', '0');
+
+    @Test
+    public void givenBlockchain_whenNewBlockAdded_thenSucess(){
+        Block newBlock = new Block(
+                "New Block",
+                blockchain.get(blockchain.size() - 1).getHash(),
+                new Date().getTime());
+        newBlock.mineBlock(prefix);
+        assertTrue(newBlock.getHash().substring(0, prefix).equals(prefixString));
+        blockchain.add(newBlock);
+    }
+
+    @Test
+    public void givenBlockchain_whenValidated_thenSucess(){
+        boolean flag = true;
+        for (int i = 0; i < blockchain.size(); i++){
+            String previousHash = i==0 ? "0" : blockchain.get(i -1).getHash();
+            flag = blockchain.get(i).getHash().equals(blockchain.get(i).calculateBlockHash())
+                    && previousHash.equals(blockchain.get(i).getPreviousHash())
+                    && blockchain.get(i).getHash().substring(0, prefix).equals(prefixString);
+                        if (!flag) break;
+        }
+        assertTrue(flag);
+    }
+
+
 }
